9783762a480d07c969fff284c7706c49b783f19a
neyaznafiz <niiyazobug@gmail.com>
2022-11-21 13:41:45 +0600

    server.js router and controller initial setup

---
+++ controller/product.controller.js
@@ -0,0 +1,4 @@
+
+exports.getProduct = async (req, res, next) => {
+    
+}
---
+++ routes/product.route.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const productController = require("../controller/product.controller");
+
+router
+  .route("/")
+  .get(productController.getProduct)
+//   .post(productController.createProduct);
